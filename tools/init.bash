27cdf2cb0535b455896beab4035d1972222b4f61
root <kireev1820@gmail.com>
2025-10-31 12:48:53 +0000

    bugfix remove extra read -p

--- tools/init.bash
+++ tools/init.bash
@@ -99,9 +99,6 @@ confirm_and_save_configuration() {
     done
     echo "-----------------------------------------------------"
     echo ""
-
-    read -p "Proceed with this configuration? (y/n): " CONFIRM
-    echo ""
     while :; do
         read -p "Proceed with this configuration? (y/n): " CONFIRM
         [[ "$CONFIRM" == "y" ]] && break
