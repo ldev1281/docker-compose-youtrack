c0f7a876d8eda564d831be171dbb79250a6d2cb1
root <kireev1820@gmail.com>
2025-10-31 14:54:27 +0000

    bugfix

--- tools/init.bash
+++ tools/init.bash
@@ -102,7 +102,7 @@ confirm_and_save_configuration() {
     while :; do
         read -p "Proceed with this configuration? (y/n): " CONFIRM
         [[ "$CONFIRM" == "y" ]] && break
-        [[ "${CONFIRM:-}" == "n" ]] && { echo "Configuration aborted by user."; exit 1; }
+        [[ "$CONFIRM" == "n" ]] && { echo "Configuration aborted by user."; exit 1; }
     done
 
     printf "%s\n" "${CONFIG_LINES[@]}" >"$ENV_FILE"
